b4f8596dff4e2be4bb6154a48025f67a1a689cc0
Chuo <ngiubing02@gmail.com>
2022-12-12 01:03:25 +0800

    feat: add aliases to sql select statements

--- prj_acnovation_a187806/frm_mainmenu_a187806.vb
+++ prj_acnovation_a187806/frm_mainmenu_a187806.vb
@@ -3,19 +3,45 @@
 Public Class frm_mainmenu_a187806
     ' Currently I'm using a general tableView template form for all the tables, but I have also
     ' created individual forms just in case there are customisation for each of them
-    Dim productsForm As New frm_tableView_a187806("Products", "SELECT * FROM TBL_PRODUCTS_A187806")
-    Dim customersForm As New frm_tableView_a187806("Customers", "SELECT * FROM TBL_CUSTOMERS_A187806")
-    Dim ordersForm As New frm_tableView_a187806("Orders", "SELECT * FROM TBL_ORDERS_A187806")
-    Dim staffsForm As New frm_tableView_a187806("Staffs", "SELECT * FROM TBL_STAFFS_A187806")
+    Dim productsForm As New frm_tableView_a187806("Products",
+        "SELECT " &
+            "FLD_PRODUCT_ID AS [ID], " &
+            "FLD_PRODUCT_NAME AS [Product Name], " &
+            "FORMAT(FLD_PRICE, 'N2') AS [Price (Sen)], " &
+            "FLD_TYPE AS [Type], " &
+            "FLD_CONDITION AS [Condition], " &
+            "FLD_DESCRIPTION AS [Description], " &
+            "FLD_STOCK AS [Stock] " &
+        "FROM TBL_PRODUCTS_A187806 "
+    )
+    Dim ordersForm As New frm_tableView_a187806("Orders",
+        "SELECT " &
+            "FLD_ORDER_ID AS [ID], " &
+            "FLD_PROCESSED_BY_STAFF_ID AS [Processed By], " &
+            "FLD_ORDERED_BY_CUSTOMER_ID AS [Customer ID] " &
+        "FROM TBL_ORDERS_A187806")
+
+    Dim customersForm As New frm_tableView_a187806("Customers",
+        "SELECT " &
+            "FLD_CUSTOMER_ID AS [ID], " &
+            "FLD_NAME AS [Name], " &
+            "FLD_CONTACT_NUMBER AS [Contact Number] " &
+        "FROM TBL_CUSTOMERS_A187806"
+    )
+    Dim staffsForm As New frm_tableView_a187806("Staffs",
+        "SELECT " &
+            "FLD_STAFF_ID AS [ID], " &
+            "FLD_NAME AS [Name] " &
+        "FROM TBL_STAFFS_A187806")
+    Private Sub btn_products_Click(sender As Object, e As EventArgs) Handles btn_products.Click
+        productsForm.Show()
+        Hide()
+    End Sub
     Private Sub btn_order_Click(sender As Object, e As EventArgs) Handles btn_order.Click
         ordersForm.Show()
         Hide()
     End Sub
 
-    Private Sub btn_products_Click(sender As Object, e As EventArgs) Handles btn_products.Click
-        productsForm.Show()
-        Hide()
-    End Sub
 
     Private Sub btn_customer_Click(sender As Object, e As EventArgs) Handles btn_customer.Click
         customersForm.Show()
